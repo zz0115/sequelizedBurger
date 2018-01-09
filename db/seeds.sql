9091cfc8f9a19ca75df2c919619aa492a56d7060
zz0115 <{zz0115}@users.noreply.github.com>
2018-01-08 20:54:11 -0700

    origin burger app

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name) VALUES ('Chicken Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Beef Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Lamb Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Sausage Burger', true);
+INSERT INTO burgers (name) VALUES ('Vegetable Burger');
